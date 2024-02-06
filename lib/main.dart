3b5ea58a9cbcd5d23cbf134247845971a3c2c1e0
ssgamage <135002341+ssgamage@users.noreply.github.com>
2024-02-06 18:42:13 +0530

    First upload

---
+++ lib/main.dart
@@ -0,0 +1,263 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  bool rememberMe = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Expanded(
+              flex: 2,
+              child: Container(
+                color: Colors.white,
+                alignment: Alignment.topCenter,
+                padding: const EdgeInsets.only(top: 70),
+                child: Image.asset('assets/images/coffee1.png', width: 220),
+              ),
+            ),
+            Expanded(
+              flex: 3,
+              child: Container(
+                color: Colors.white, // Adjust color as needed
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    const Padding(
+                      padding:
+                          EdgeInsets.symmetric(vertical: 25, horizontal: 20),
+                      child: Text(
+                        'Welcome back',
+                        style: TextStyle(
+                          fontSize: 30,
+                          color: Colors.pink,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                    Form(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Padding(
+                            padding: const EdgeInsets.symmetric(horizontal: 15),
+                            child: TextFormField(
+                              keyboardType: TextInputType.emailAddress,
+                              decoration: const InputDecoration(
+                                labelText: 'Email/Phone Number',
+                                prefixIcon: Icon(Icons.email),
+                                border: UnderlineInputBorder(),
+                              ),
+                              onChanged: (String value) {},
+                              validator: (value) {
+                                return value!.isEmpty
+                                    ? 'Please enter email or phone number'
+                                    : null;
+                              },
+                            ),
+                          ),
+                          const SizedBox(
+                            height: 20,
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.symmetric(horizontal: 15),
+                            child: TextFormField(
+                              keyboardType: TextInputType.visiblePassword,
+                              obscureText: true,
+                              decoration: const InputDecoration(
+                                labelText: 'Password',
+                                prefixIcon: Icon(Icons.lock),
+                                suffixIcon: null, // Remove the info icon
+                                border: UnderlineInputBorder(),
+                              ),
+                              onChanged: (String value) {},
+                              validator: (value) {
+                                return value!.isEmpty
+                                    ? 'Please enter password'
+                                    : null;
+                              },
+                            ),
+                          ),
+                          Row(
+                            children: [
+                              Padding(
+                                padding: const EdgeInsets.symmetric(
+                                    horizontal: 10, vertical: 10),
+                                child: Row(
+                                  children: [
+                                    Checkbox(
+                                      value: rememberMe,
+                                      onChanged: (bool? value) {
+                                        setState(() {
+                                          rememberMe = value!;
+                                        });
+                                      },
+                                      fillColor: MaterialStateProperty
+                                          .resolveWith<Color?>(
+                                        (Set<MaterialState> states) {
+                                          if (states.contains(
+                                              MaterialState.selected)) {
+                                            return Colors.pink; // Checked color
+                                          } else if (states.contains(
+                                              MaterialState.hovered)) {
+                                            return Colors.pink.withOpacity(
+                                                0.04); // Hovered color
+                                          }
+                                          return null;
+                                        },
+                                      ),
+                                    ),
+                                    const Text(
+                                      'Remember me',
+                                      style: TextStyle(color: Colors.pink),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                              const Spacer(),
+                              InkWell(
+                                onTap: () {
+                                  //logic for "Forgot Password?" link
+                                },
+                                child: const Padding(
+                                  padding: EdgeInsets.symmetric(horizontal: 15),
+                                  child: Text(
+                                    'Forgot Password?',
+                                    style: TextStyle(
+                                      color: Colors.pink,
+                                    ),
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            Expanded(
+              flex: 2,
+              child: Container(
+                color: Colors.white, // Adjust color as needed
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    // First Line: Login Button
+                    Padding(
+                      padding: const EdgeInsets.symmetric(
+                          horizontal: 40.0, vertical: 5.00),
+                      child: Align(
+                        alignment: Alignment.topCenter,
+                        child: ElevatedButton(
+                          onPressed: () {},
+                          style: ElevatedButton.styleFrom(
+                            foregroundColor: Colors.white,
+                            backgroundColor: Colors.pink,
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(20.0),
+                            ),
+                            minimumSize: const Size(double.infinity, 50),
+                          ),
+                          child: const Text(
+                            'Login',
+                            style: TextStyle(fontSize: 23),
+                          ),
+                        ),
+                      ),
+                    ),
+
+                    // Second Line: Text
+                    const Padding(
+                      padding: EdgeInsets.all(10.0),
+                      child: Text(
+                        'Or Login with',
+                        style: TextStyle(fontSize: 16),
+                      ),
+                    ),
+
+                    // Third Line: Three Images
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.all(15.0),
+                          child: Image.asset('assets/images/facebooklogo2.png',
+                              width: 40, height: 40),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.all(15.0),
+                          child: Image.asset('assets/images/instagramlogo1.png',
+                              width: 40, height: 40),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.all(15.0),
+                          child: Image.asset('assets/images/linkedinlogo1.png',
+                              width: 40, height: 40),
+                        ),
+                      ],
+                    ),
+
+                    // Fourth Line: Text
+                    Padding(
+                      padding: const EdgeInsets.all(10.0),
+                      child: GestureDetector(
+                        onTap: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => const MyApp(),
+                            ),
+                          );
+                        },
+                        child: RichText(
+                          text: const TextSpan(
+                            children: [
+                              TextSpan(
+                                text: "Don't have account?",
+                                style: TextStyle(
+                                  fontSize: 16,
+                                  color: Colors.black,
+                                ),
+                              ),
+                              TextSpan(
+                                text: ' Sign up',
+                                style: TextStyle(
+                                  fontSize: 16,
+                                  color: Colors.pink,
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+        resizeToAvoidBottomInset: false,
+      ),
+    );
+  }
+}

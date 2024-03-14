b3905f654fa14eb262afd9091f6ac14a325ba322
sachiladilhara <sachiladilhara7@gmail.com>
2024-03-14 18:08:10 +0530

    Name re edited

--- lib/main.dart
+++ lib/main.dart
@@ -44,7 +44,7 @@ class _MyAppState extends State<MyApp> {
                           EdgeInsets.symmetric(vertical: 25, horizontal: 20),
                       child: Text(
                         //Demo Edit for test 'Welcome back'
-                        'Welcome Sachila',
+                        'Welcome Dilhara',
                         style: TextStyle(
                           fontSize: 30,
                           color: Colors.pink,
